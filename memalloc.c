0ca963502152460e2f5fe21a504113a89c36b374
prachyo <prachyosarker565@gmail.com>
2022-11-28 16:32:36 -0500

    implemented calloc()

--- memalloc.c
+++ memalloc.c
@@ -117,3 +117,20 @@ void *malloc(size_t size)
 	pthread_mutex_unlock(&global_malloc_lock);
 	return (void*)(header + 1);
 }
+
+void *calloc(size_t num, size_t nsize)
+{
+	size_t size;
+	void *block;
+	if (!num || !nsize)
+		return NULL;
+	size = num * nsize;
+	/* check mul overflow */
+	if (nsize != size / num)
+		return NULL;
+	block = malloc(size);
+	if (!block)
+		return NULL;
+	memset(block, 0, size);
+	return block;
+}
