ccd17586e36b4c946ace6885192c631c8bb141e3
prachyo <prachyosarker565@gmail.com>
2022-11-28 16:31:27 -0500

    helper for free()

--- memalloc.c
+++ memalloc.c
@@ -19,3 +19,15 @@ typedef union header header_t;
 
 header_t *head = NULL, *tail = NULL;
 pthread_mutex_t global_malloc_lock;
+
+header_t *get_free_block(size_t size)
+{
+	header_t *curr = head;
+	while(curr) {
+		/* see if there's a free block that can accomodate requested size */
+		if (curr->s.is_free && curr->s.size >= size)
+			return curr;
+		curr = curr->s.next;
+	}
+	return NULL;
+}
